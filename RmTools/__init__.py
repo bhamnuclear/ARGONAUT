2f07735fdf3a1408da272bcb48f382c48532a283
Alex Brooks <alex@brooksontheweb.com>
2025-09-16 12:32:45 +0100

    Created init file

---
+++ RmTools/__init__.py
@@ -0,0 +1,10 @@
+"""
+ARGONAUT.
+
+R-matrix sequential decay strength profile tool
+"""
+from RmTools import Argonaut
+
+__version__ = "1.0.0"
+__author__ = "Alex Brooks"
+__credits__ = "University of Birmingham Nuclear Physics Group"
